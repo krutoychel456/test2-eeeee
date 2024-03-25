4dbf660b156d283cecd87d30d5c640caedc4b39c
krutoychel456 <timur_prok@icloud.com>
2024-03-25 19:49:07 +0300

    v0.1

---
+++ image_parser.py
@@ -0,0 +1,18 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+query =  input("Введите запрос: ")
+
+url = f"https://www.kiddle.co/s.php?q={query}"
+
+page = requests.get(url).text
+soup = BeautifulSoup(page, "html.parser")
+
+for raw_img in soup.find_all("img"):
+    link = raw_img.get("src")
+    if link and link.startswith("https"):
+        response = requests.get(link)
+        with open("./today_avatar.jpg", "wb") as f:
+            f.write(response.content)
+        break
